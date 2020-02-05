b177b94d08bb47a78f382b85b4fbc0010a8a3b72
Richard.Winterhalder <richie676@gmail.com>
2020-02-05 21:11:47 +0100

    init commit

---
+++ PlaygroundApi/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlaygroundApi.DTO;
+
+namespace PlaygroundApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public JsonResult Get(string identcode)
+        {
+
+            var user = new UserDto() { firstName = "Gertraude", lastName = "Get", userName = "Gerti"};
+
+            return Json(user);
+        }
+
+        [HttpPost]
+        public JsonResult Post(string email)
+        {
+            var user = new UserDto() { firstName = "Paul", lastName = "Post", userName = "Pauli" };
+
+            return Json(user);
+        }
+
+        [HttpPut]
+        public JsonResult Put(string identcode, string newStatus)
+        {
+            var user = new UserDto() { firstName = "Peter", lastName = "Put", userName = "Peet" };
+
+            return Json(user);
+        }
+    }
+}
---
+++ PlaygroundApi/DTO/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlaygroundApi.DTO
+{
+    public class UserDto
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string userName { get; set; }
+    }
+}
